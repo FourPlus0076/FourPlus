c069e9614d992a90f7e05895dfdcbb697ab0ccb8
gausm <gausm@LAPTOP-91RABLC6>
2022-12-27 11:48:53 +0530

    Add project files.

---
+++ FourPlus/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FourPlus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
